24dc6cac6eb80132cd73cf0edc7168393252d817
abhishekkumar-coding <abhiwebdev2.0@gmail.com>
2025-02-27 09:30:19 -0500

    updated resume

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,34 +1,67 @@
-"use client"
-import React from 'react'
-import "../globals.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+"use client";
+import React, { useState, useEffect } from "react";
+import "../globals.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
-interface IPROPS{
-    toggle :  () => void,
+interface IPROPS {
+  toggle: () => void;
 }
 
-const Navbar : React.FC<IPROPS> = ({toggle}) =>{
-    return (
-        <div className='w-full px-2 md:px-0 sticky top-0 z-50 bg-[#26093d]'>
-            <div className='mx-auto max-w-6xl py-3 flex justify-between items-center'>
-                <h1 className='text-white text-4xl font-semibold py-2 cursor-pointer'><a href="#home">AbhiDev</a></h1>
-               <div className='flex items-center gap-3' > 
-                <div className='flex items-center justify-center'>
-                    <ul className='hidden md:flex space-x-10 text-xl items-center text-white'>
-                        <li className='customBorder cursor-pointer'><a href="#home">Home</a> </li>
-                        <li className='customBorder cursor-pointer'><a href="#skills">Skills </a></li>
-                        <li className='customBorder cursor-pointer'><a href="#project">Projects</a> </li>
-                        <li className='customBorder cursor-pointer'><a href="#service">Services</a> </li>
-                        <li className='customBorder cursor-pointer'><a href="#about">About </a></li>
-                    </ul>
-                    <button className='py-2 px-3 md:ml-10 bg-white hover:bg-gray-200 transition-all duration-200 text-[#1E40AF] font-semibold text-xl rounded-lg'><a href="#contact">Hire Me</a></button>
-                </div>
-                <button onClick={toggle} className='text-4xl md:hidden'><FontAwesomeIcon icon={faBars} style={{color: "#ffffff",}} /></button>
-            </div>
-            </div>
+const Navbar: React.FC<IPROPS> = ({ toggle }) => {
+  const [showNav, setShowNav] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 90) {
+        setShowNav(true);
+      } if(window.scrollY < 90) {
+        setShowNav(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+  }, []);
+
+  console.log(showNav);
+
+  return (
+    <div className={`w-full px-2 md:px-0 sticky top-0 transition-all duration-100 ${showNav ? "bg-opacity-100" : "bg-opacity-0"} z-50 bg-[#26093d]`}>
+      <div className="mx-auto max-w-6xl py-3 flex justify-between items-center">
+        <h1 className="text-white text-4xl font-semibold py-2 cursor-pointer">
+          <a href="#home">AbhiDev</a>
+        </h1>
+        <div className="flex items-center gap-3">
+          <div className="flex items-center justify-center">
+            <ul className="hidden md:flex space-x-10 text-xl items-center text-white">
+              <li className="customBorder cursor-pointer">
+                <a href="#home">Home</a>
+              </li>
+              <li className="customBorder cursor-pointer">
+                <a href="#skills">Skills</a>
+              </li>
+              <li className="customBorder cursor-pointer">
+                <a href="#project">Projects</a>
+              </li>
+              <li className="customBorder cursor-pointer">
+                <a href="#service">Services</a>
+              </li>
+              <li className="customBorder cursor-pointer">
+                <a href="#about">About</a>
+              </li>
+            </ul>
+            <button className="py-2 px-3 md:ml-10 bg-white hover:bg-gray-200 transition-all duration-200 text-[#1E40AF] font-semibold text-xl rounded-lg">
+              <a href="#contact">Hire Me</a>
+            </button>
+          </div>
+          <button onClick={toggle} className="text-4xl md:hidden">
+            <FontAwesomeIcon icon={faBars} style={{ color: "#ffffff" }} />
+          </button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
